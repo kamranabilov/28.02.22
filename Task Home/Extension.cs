633638514c95df49a55adf4fe3e1253fecc36a2d
kamranabilov <kamrangab@code.edu.az>
2022-02-28 22:27:01 +0400

    added Task

---
+++ Task Home/Extension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_Home
+{
+    static class Extension
+    {
+        public static string IsCapitalized(this string word)
+        {
+            string text = " ";
+            word = word.ToLower();
+            string[] words = word.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i][0].ToString().ToUpper() + words[i].Substring(1);
+            }
+            text = string.Join(" ", words);
+            return text;
+        }
+
+
+        public static bool FindWord(this string words, string search)
+        {
+            string[] word = words.Split(' ');
+            foreach (string item in word)
+            {
+                if (item == search)
+                {
+                    return true;
+                }                
+            }
+            return false;
+        }
+    }
+}
---
+++ Task Home/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wrongstr = "exTensiON claSSi staTIc OLMalIdiR";            
+            Console.WriteLine(wrongstr.IsCapitalized());
+
+
+            string words = "Hello How are You.";
+            Console.WriteLine(words.FindWord("are"));
+
+        }
+    }
+}
